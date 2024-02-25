dcad6f6fe099670ed748f350d50fe044dd361e27
Jaydrey <jaribwetshi64@gmail.com>
2024-02-26 00:16:59 +0300

    feat(models): added a custom user model for the user app

--- src/django/users/models.py
+++ src/django/users/models.py
@@ -1,3 +1,74 @@
+from uuid import uuid4
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, **extra_fields):
+        try:
+            user: User = self.model(**extra_fields)
+            user.set_password(extra_fields.get("password"))
+            user.save(using=self._db)
+            return user
+        except Exception as e:
+            print(f"Error while creating user {e}")
+            raise ValueError(e)
+
+    def create_superuser(self, email: str = None, password: str = None, phone_number: str = None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email=email, password=password, phone_number=phone_number,**extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    GENDER_CHOICES = (
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    )
+
+
+    ROLE_CHOICES = (
+        ("project_manager", "ProjectManager"),
+        ("developer", "Developer"),
+    )
+    class Meta:
+        default_related_name = _("users")
+        indexes = (
+            models.Index(fields=("id", "email")),
+        )
+        ordering = ("-created_at",)
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    id = models.UUIDField(_("user id"), default=uuid4,
+                          editable=False, primary_key=True)
+    email = models.EmailField(_("email"),unique=True)
+    phone_number = models.CharField(
+        _("phone number"), max_length=10,)
+    first_name = models.CharField(_("first name"), max_length=50, null=True, blank=True)
+    last_name = models.CharField(_("last name"), max_length=50, null=True, blank=True)
+    profile_picture = models.URLField(_("profile picture"), null=True, blank=True)
+    date_of_birth = models.DateField(_("date of birth"), null=True, blank=True)
+    gender = models.CharField(_("gender"), max_length=10, choices=GENDER_CHOICES, default="male")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="project_manager")
+    is_active = models.BooleanField(_("is active"), default=False)
+    is_superuser = models.BooleanField(_("is super user"), default=False)
+    is_staff = models.BooleanField(_("is staff"), default=False)
+    is_deleted = models.BooleanField(_("is deleted"), default=False)
+    created_at = models.DateTimeField(
+        _("date created"), default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(
+        _("edited date"), editable=False, default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["phone_number",]
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return f"{self.email}"
+
