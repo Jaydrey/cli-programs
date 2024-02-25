4f128747cf734883af5802ab81e15e070d393f66
Jaydrey <jaribwetshi64@gmail.com>
2024-02-26 00:09:13 +0300

    feat(views): added the login view of simple jwt token

--- src/django/users/views.py
+++ src/django/users/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import serializers
+
+
+from rest_framework.permissions import AllowAny
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import (
+    UserSerializer,
+    UserLoginSerializer,
+)
+# swagger
+from drf_spectacular.utils import (
+    extend_schema,
+)
+
+class LoginAPIView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    @extend_schema(
+        request=UserLoginSerializer,
+        responses={
+            400: str,
+            401: str,
+            200: dict[str, str],
+        },
+    )
+    def post(self, request: Request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError as e:
+            if "invalid_password" in e.detail:
+                return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+            if "invalid_email" in e.detail:
+                return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+            if "invalid_credentials" in e.detail:
+                return Response({"errors": e.detail}, status=status.HTTP_401_UNAUTHORIZED)
+
+            return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        user = serializer.validated_data.get("user")
+
+        refresh = RefreshToken.for_user(user)
+
+        refresh["email"] = str(user.email)
+
+        return Response({
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }, status=status.HTTP_200_OK)
+
+
+
